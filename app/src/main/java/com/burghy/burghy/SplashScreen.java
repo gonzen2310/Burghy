221866b5746fa219fed383613e97d482763f146b
Gonzen2310 <gonzaloreyesllauca@gmail.com>
2017-10-10 17:09:10 -0400

    Add Splash Screen

---
+++ app/src/main/java/com/burghy/burghy/SplashScreen.java
@@ -0,0 +1,26 @@
+package com.burghy.burghy;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private static int SPLASH_TIME = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent loginIntent = new Intent (SplashScreen.this, MainActivity.class);
+                startActivity(loginIntent);
+                finish();
+            }
+        }, SPLASH_TIME);
+    }
+}
